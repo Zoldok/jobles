2e4aa020f893d4f67cdc87643fdab6a15a97fd71
Zoldok <max_kyrtimov@mail.ru>
2024-03-16 17:42:46 +0300

    fix

--- close.js
+++ close.js
@@ -3,12 +3,10 @@ const closePopUp = document.getElementById('pop_up_close')
 const popUp = document.getElementById('pop_up')
 
 openPopUp.addEventListener('click', function (e) {
-  console.log('klick')
   e.preventDefault()
   popUp.classList.add('active')
 })
 
 closePopUp.addEventListener('click', () => {
-  console.log('close')
   popUp.classList.remove('active')
 })
